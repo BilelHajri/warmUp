dfc1efcd7d3e6b0209e79eeb1ae4a7b9a0b492e9
bilel <hb.bilel@gmail.com>
2019-11-02 09:49:30 +0100

    warmup 14 done

--- warmUp14.js
+++ warmUp14.js
@@ -19,5 +19,25 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
 */
 
 var maxProfit = function(prices) {
+
+	var min = Math.min(...prices)
+	var indexMin = prices.indexOf(min)
+
+	var tab = prices.slice(indexMin)
+
+	console.log(tab)
+	max = Math.max(...tab)
+
+	var indexMax = prices.indexOf(max)
+	console.log(max)
+	console.log(indexMax)
+
+	if (max > min && indexMax > indexMin){
+		console.log("buy on day ," + (indexMin+1) + " and sell on "+ (indexMax + 1))
+		return "your profit = " + (max - min)
+	}else {
+		return "no transaction is done "
+	}
+
   // YOUR CODE HERE
 };
