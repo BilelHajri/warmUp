887b1dfb94fc7798f5c4e3ec86b48c77a4ee8b2e
bilel <hb.bilel@gmail.com>
2019-11-06 09:57:26 +0100

    warmup not done !

--- warmUp16.js
+++ warmUp16.js
@@ -6,7 +6,7 @@
 // Output: 1
 
 // Example 2:
-// Input: text = "loonbalxballpoon"
+// Input: text = "loonbalxballpoon h"
 // Output: 2
 
 // Example 3:
@@ -16,3 +16,30 @@
 // Constraints:
 // 1 <= text.length <= 10^4
 // text consists of lower case English letters only.
+
+function check(str){
+	var obj = {
+		b:0,
+		a:0,
+		l:0,
+		l:0,
+		o:0,
+		o:0,
+		n:0
+	}
+	var nb = 0
+	//var ch = ""
+	
+	for (var i= 0; i < str.length; i++){
+		for(key in obj){
+			//console.log(obj[key])
+			if(str[i] === key){
+				obj[key] +=1
+				nb +=1
+			}
+		}
+	}
+	// console.log(nb/7)
+	//7 is the total number of str
+	return nb/7
+}
