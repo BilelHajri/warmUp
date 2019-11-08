6fa2f05bbe87075c4ec826f4aeeb541b321a71f4
bilel <hb.bilel@gmail.com>
2019-11-08 10:01:16 +0100

    not done for all exemple

--- warmUp17.js
+++ warmUp17.js
@@ -1,6 +1,8 @@
 // There is a special keyboard with all keys in a single row.
 // ​
-// Given a string keyboard of length 26 indicating the layout of the keyboard (indexed from 0 to 25), initially your finger is at index 0. To type a character, you have to move your finger to the index of the desired character. The time taken to move your finger from index i to index j is |i - j|.
+// Given a string keyboard of length 26 indicating the layout of the keyboard (indexed from 0 to 25), initially 
+//your finger is at index 0. To type a character, you have to move your finger to the index of the desired character. 
+//The time taken to move your finger from index i to index j is |i - j|.
 // ​
 // You want to type a string word. Write a function to calculate how much time it takes to type it with one finger.
 // ​
@@ -27,6 +29,31 @@
 
 // ​
 // ​
-// var calculateTime = function(keyboard, word) {
-    
-// };
+var calculateTime = function(keyboard, word) {
+  var keyBoardObj = {};
+  var index = 0;
+  var result = 0;
+
+  for (var i = 0; i < keyboard.length; i++){ // create an obj of keybord word
+      keyBoardObj[keyboard[i]] = i;
+  }
+  
+
+  for (var i = 0; i < word.length; i++){ // iterate word
+
+      var ch = word[i]
+      result += keyBoardObj[ch]
+
+      if(result > index){
+      	keyBoardObj[ch] - index
+      }else{
+      	index - keyBoardObj[ch]
+      }
+ 
+      index = keyBoardObj[ch];
+
+  }
+  return result;
+};
+
+
